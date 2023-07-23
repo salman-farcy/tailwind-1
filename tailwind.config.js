36ae9b65e09ea853001936528ff2f65b42b66b1b
salman-farcy <salmanfarcy253@gmail.com>
2023-07-23 15:47:47 +0600

    file edit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+
+  daisyui: {
+    themes: ["light", "dark", "cupcake"],
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
